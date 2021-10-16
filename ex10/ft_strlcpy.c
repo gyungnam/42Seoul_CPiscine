707931670c803581c2d280b44ae60deac3fbb558
ginam <ginam@student.42seoul.kr>
2021-10-16 21:17:21 +0900

    first

---
+++ ex10/ft_strlcpy.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ginam <ginam@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/16 15:53:31 by ginam             #+#    #+#             */
+/*   Updated: 2021/10/16 15:56:08 by ginam            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	res;
+
+	i = 0;
+	res = 0;
+	while (src[res])
+		res++;
+	if (size < 1)
+		return (res);
+	while (src[i] && i < size - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (res);
+}
